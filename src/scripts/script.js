eb2d1028207c91df3ead7ef487cfb456e365904b
Дмитрий Жур <tehzienergy@gmail.com>
2016-09-14 02:03:59 +0300

    release v1.0

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -11,15 +11,15 @@ $(document).ready(function() {
   
   var imageIndex = 0;
   var imagesArray = [
-      "http://placehold.it/1919x1079/000000",
-      "http://placehold.it/1921x1081/000000",
-      "http://placehold.it/1920x1080/000000"
+      "../img/main-bg-2.jpg",
+      "../img/main-bg-3.jpg",
+      "../img/main-bg-1.jpg"
   ];
 
   function changeBackground(){
       var index = imageIndex++ % imagesArray.length;
       $(".slider-main").hide();
-      $(".slider-main").css("background","url('"+ imagesArray[index] + "') no-repeat center");
+      $(".slider-main").css("background","url('"+ imagesArray[index] + "') no-repeat left center");
       $(".slider-main").fadeIn(3000);
   }
   
