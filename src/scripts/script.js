3a622872987f1eb57af3364cf8ce6d305bab78cd
Дмитрий Жур <tehzienergy@gmail.com>
2016-09-13 16:37:35 +0300

    change slider

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,16 +1,20 @@
-$(document).ready(function() {  
+$(document).ready(function() {
   
-  $('.slider-main').slick({
-    infinite: true,
-    dots: false,
-    arrows: false,
-    fade: true,
-    cssEase: 'linear',
-    autoplay: true,
-    autoplaySpeed: 5000,
-    touchMove: false,
-    swipe: false,
-  });
+  var imageIndex = 0;
+  var imagesArray = [
+      "http://placehold.it/1919x1079/000000",
+      "http://placehold.it/1921x1081/000000",
+      "http://placehold.it/1920x1080/000000"
+  ];
+
+  function changeBackground(){
+      var index = imageIndex++ % imagesArray.length;
+      $(".slider-main").hide();
+      $(".slider-main").css("background","url('"+ imagesArray[index] + "') no-repeat center");
+      $(".slider-main").fadeIn(3000);
+  }
+  
+  setInterval(changeBackground, 8000);
   
   if ($(window).width() < 991) {
     $('.menu__button').fancybox();
